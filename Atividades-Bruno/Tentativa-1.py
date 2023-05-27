d0420dabcd632141d51d15991d26bfc50b4221d0
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-26 21:10:24 -0300

    a

--- Atividades-Bruno/Tentativa-1.py
+++ Atividades-Bruno/Tentativa-1.py
@@ -1,49 +1,27 @@
-import random
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
 
+# Carregar o conjunto de dados MNIST
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-num1 = random.randrange(1,99)
-num2 = random.randrange(1,99)
-num3 = random.randrange(1,99)
-num4 = random.randrange(1,99)
-letr1 = "ad"
-letr2 = "po"
-letr3 = "as"
-letr4 = "gb"
-letr5 = "jh"
-letr6 = "ijsdsa"
-letr7 = "iu"
-letr8 = "as"
+# Pré-processamento dos dados
+x_train = x_train / 255.0
+x_test = x_test / 255.0
 
+# Criar o modelo da rede neural
+model = Sequential()
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 
+# Compilar o modelo
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+# Treinar o modelo
+model.fit(x_train, y_train, epochs=5, batch_size=32, verbose=1)
 
-if num1 < 50:
-    
-    junc1 = num1,letr1
-    
-junc1 = num1,letr2
-
-
-if num2 < 50:
-    
-    junc2 = num2,letr3
-    
-junc2 = num2,letr4
-
-
-if num3 < 50:
-    
-    junc3 = num3,letr5
-    
-junc3 = num3,letr6
-
-
-if num4 < 50:
-    
-    junc4 = num4,letr7
-    
-junc4 = num4,letr8
-
-
-junc5 = +junc49
-
+# Avaliar o modelo
+loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+print('Acurácia no conjunto de teste:', accuracy)
