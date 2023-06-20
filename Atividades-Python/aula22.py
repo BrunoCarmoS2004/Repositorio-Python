c260461841419e0324c988639ac88148c8fbb60b
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-06-20 16:14:58 -0300

    Create aula22.py

---
+++ Atividades-Python/aula22.py
@@ -0,0 +1,33 @@
+
+
+
+from logging import exception
+import os
+
+
+os.system('cls')
+
+try:
+    print(x)
+except:
+    print("Deu erro: o  código X está errado ")
+else:
+    print("Tudo certo")
+    
+    
+
+y = 10
+try:
+    print(y)
+except:
+    print("Deu erro: o  código Y está errado ")
+else:
+    print("Tudo certo")
+    
+    
+num=0
+
+if num < 1:
+    raise Exception("CODIGO DE IA N DEU CERTO")
+
+print("bababa")
